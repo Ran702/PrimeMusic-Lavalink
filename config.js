ccd82298568f6bbd64f63e89605a31534f5224a9
Ran702 <125109961+Ran702@users.noreply.github.com>
2025-03-06 21:22:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["527446814832459776", ""], 
   mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  spotifyClientId : "806f99cc475a414a9c576c2cc7cf5dda",
+  spotifyClientSecret : "c1355af45ad846c081d0e66ebbc2a9dd",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
@@ -14,13 +14,13 @@ module.exports = {
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
-  errorLog: "", 
+  errorLog: "https://discord.com/api/webhooks/1343189121518145618/Y9BLBKfNWweKa3iRFNa5smfGLrD5NDuUcCMA-UT6Ez-aUhcHtBl9NKcDuWoWRhmPPSK5", 
   nodes: [
      {
-      name: "XX",
-      password: "enteryourcustompass",
-      host: "180.188.226.76",
-      port:  7019,
+      name: "Public Lavalink v4 NonSSL",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port:  80,
       secure: false
     }
   ]
